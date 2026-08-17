b87bde2a0f30c9bf5674d1de9acd0840a738231b
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-08-17 12:19:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='rafiq-quran-v58';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])).catch(()=>{}))});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match('./index.html')))});
